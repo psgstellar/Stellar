d670469bd590acecd7489afe85e6f4be799dc403
enfycius <hexk0131@gmail.com>
2021-03-25 21:41:06 +0900

    fix Issue #53

--- PyStellar/stellar/Git/service/git_commit_service.py
+++ PyStellar/stellar/Git/service/git_commit_service.py
@@ -10,28 +10,35 @@ class GitCommitCheckService:
     def git_public_request(cls, request):
         """Commit 기록 요청"""
         print('-------request-------', request)
-        owner = request['github_name']
-        repo = request['github_repo']
-        if request.get('since') and request.get('until'):
-            # date_params = {'since': request['since'],
-            #                'until': request['until']}
-            since = request['since']
-            until = request['until']
-            r = requests.get(
-                f'https://api.github.com/repos/{owner}/{repo}/commits?my_client_id={owner}?since={since}?until={until}')
-        elif request.get('since'):
-            # date_params = {'since': request['since']}
-            since = request['since']
-            r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits?my_client_id={owner}?since={since}')
+        owner = request.GET['owner']
+        repo = request.GET['repo']
 
-        elif request.get('until'):
-            # date_params = {'until': request['until']}
-            until = request['until']
-            r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits?my_client_id={owner}?until={until}')
+        # if request.GET('since') and request.GET('until'):
+        #     # date_params = {'since': request['since'],
+        #     #                'until': request['until']}
+        #     since = request.GET['since']
+        #     until = request.GET['until']
+        #     r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits?my_client_id={owner}?since={since}?until={until}')
+        # elif request.GET('since'):
+        #     # date_params = {'since': request['since']}
+        #     since = request.GET['since']
+        #     r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits?my_client_id={owner}?since={since}')
 
-        else:
-            r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits?my_client_id={owner}')
+        # elif request.GET('until'):
+        #     # date_params = {'until': request['until']}
+        #     until = request.GET['until']
+        #     r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits?my_client_id={owner}?until={until}')
+
+        # else:
+        r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits?my_client_id={owner}')
+
+        print(r)
+
         data = r.json()
+
+        print(data)
+
+        
         # commit_list = []
         if len(data) == 0:
             commit_json = {'message': '오늘 커밋 없음'}
@@ -52,6 +59,7 @@ class GitCommitCheckService:
                                     'message': data['message'],
                                     'url': data['documentation_url']})
                 # commit_list.append(commit_json)
+
         return commit_json
 
     @classmethod
